a7c9c8177d73bbbbae807d5246ae4ed5fc7392cf
Lee Brooks <lee@kagiso.io>
2015-11-05 11:46:33 +0200

    Initial check in

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='kagiso_image',
+    version='0.0.1',
+    author='Kagiso Media',
+    author_email='development@kagiso.io',
+    description='Kagiso Image with Attribution',
+    url='https://github.com/Kagiso-Future-Media/kagiso_image',
+    packages=find_packages()
+)
