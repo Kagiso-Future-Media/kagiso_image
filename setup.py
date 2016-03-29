07b159b9ea6f46431b9cd80cdfe2b6077f5c2cd2
Mhlengi Vincent Mzimela <mhlengij5@gmail.com>
2016-03-29 10:09:12 +0200

    Changed version number from `0.0.3` to `0.0.4`.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='kagiso_image',
-    version='0.0.3',
+    version='0.0.4',
     author='Kagiso Media',
     author_email='development@kagiso.io',
     description='Kagiso Image with Attribution',
